10484d76fc603194c924be736dd51b6c3350b855
laneloot <laneloot03@gmail.com>
2025-10-07 06:59:44 -0500

    refactor: convert polls app into a package

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'debug_toolbar',
-    'polls.apps.PollsConfig',
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
